766bc147ef87ebf7988fc43d4de16ce9a44f60d4
Salim <salimabdul.it@gmail.com>
2020-02-07 20:26:37 +0200

    Small refactor

--- int.lua
+++ int.lua
@@ -10,7 +10,7 @@ local gpu = component.gpu
 local data = component.database
 local tunnel = component.tunnel
 
-require("db")
+require("durexdb")
 local db = Database:new()
 local transposerAddresses = {}
 local storageAddresses = {}
