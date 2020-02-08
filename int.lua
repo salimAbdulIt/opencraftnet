6d4971ec282bc7ecd1b5ee81491b7b8a18a8c088
Salim <salimabdul.it@gmail.com>
2020-02-08 15:33:58 +0200

    Small refactor

--- int.lua
+++ int.lua
@@ -29,7 +29,7 @@ local id_of_available_slot = 'minecraftair_0'
 local nameOfRobot = 'opencomputers:robot'
 local nameOfChest = 'tile.chest'
 local order = {}
-local storages = { ["tile.IronChest"] = 'storage', ["tile.DiamondChest"] = 'storage', ['opencomputers:robot'] = 'robot' }
+local storages = { ["tile.IronChest"] = 'storage', ['opencomputers:robot'] = 'robot' }
 local findNameFilter
 
 local revercedAddresses = {}
@@ -102,7 +102,7 @@ function findEnd(address, lastOutputTransposer)
                             storageAddresses[address1].outputSide = outputSide
                             storageAddresses[address1].inputSide = inputSide
                             storageAddresses[address1].ignoreFirstSlot = false
-                            if (transposerAddresses[address].transposer.transferItem(inputSide, outputSide, 64, 1, 1)) then
+                            if (transposerAddresses[address].transposer.transferItem(inputSide, outputSide, 64, 1, 1) == 0) then
                                 storageAddresses[address1].ignoreFirstSlot = true
                                 findEnd(address .. outputSide, transposerAddresses[address].transposer.address)
                                 transposerAddresses[address].transposer.transferItem(outputSide, inputSide, 64, 1, 1)
