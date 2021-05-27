2f8f4e461b4197085b38af4811a6f6195db92cb9
Salim <salimabdul.it@gmail.com>
2021-05-27 17:44:42 +0300

    Remove custom serializable

--- v2/transposers-tests.lua
+++ v2/transposers-tests.lua
@@ -9,19 +9,19 @@ require('transposers')
 local transposers = Transposers:new()
 local Tests = {}
 
-local interfaceCoords = {"x": 558,"y":7,"z":624}
+local interfaceCoords = {["x"]= 558, ["y"]=7,["z"]=624}
 local chestName = "minecraft:chest"
 local chestDmg = 0
 
 local chests = {
-   {"x": 0,"y":0,"z":0},
-   {"x": -1,"y":-1,"z":0},
-   {"x": 0,"y":-2,"z":0},
-   {"x": 1,"y":-3,"z":0},
-   {"x": -1,"y":-3,"z":0},
-   {"x": 3,"y":-3,"z":0},
-   {"x": -3,"y":-3,"z":0},
-   {"x": 2,"y":-2,"z":0}
+   {["x"]= 0,["y"]=0,["z"]=0},
+   {["x"]= -1,["y"]=-1,["z"]=0},
+   {["x"]= 0,["y"]=-2,["z"]=0},
+   {["x"]= 1,["y"]=-3,["z"]=0},
+   {["x"]= -1,["y"]=-3,["z"]=0},
+   {["x"]= 3,["y"]=-3,["z"]=0},
+   {["x"]= -3,["y"]=-3,["z"]=0},
+   {["x"]= 2,["y"]=-2,["z"]=0}
 }
 local function init()
     local world = debug.getWorld(0)
