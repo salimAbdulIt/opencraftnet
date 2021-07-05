e85a6a9ef5a0e61028db99330febdf32b727762d
Salim <salimabdul.it@gmail.com>
2021-07-05 19:50:57 +0300

    Remove custom serializable

--- v2/thaumcraft/autofill.lua
+++ v2/thaumcraft/autofill.lua
@@ -25,7 +25,7 @@ function ThaumAutofill:new()
             local key = item["name"] .. ':' .. item["damage"]
             local itemFromChest = groupedItems[key]
 
-            if (not itemFromChest or itemFromChest.count < item.count) then
+            if (not itemFromChest or itemFromChest.size < item.count) then
                 error("dont have item")
             end
         end
