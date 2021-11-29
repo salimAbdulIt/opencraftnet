d54b7b1a406390ac1df4664d6cb06e31fa0c5398
Durex77 <salimabdul.it@gmail.com>
2021-11-29 20:16:20 +0200

    add shop

--- shop/install.lua
+++ shop/install.lua
@@ -1,10 +1,11 @@
 local REPOSITOTY = "https://gitlab.com/lfreew1ndl/opencraftnetoriginal/-/raw/dev"
 
 local shell = require("shell")
+shell.execute("wget -fq " .. REPOSITOTY .. "/shop/forms.lua /home/lib/forms.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/shop/shop.lua /home/1.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/shop/shopService.lua lib/shopService.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/v2/database.lua lib/database.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/v2/discord-logger.lua lib/dlog.lua")
+shell.execute("wget -fq " .. REPOSITOTY .. "/v2/libs/discord-logger.lua lib/dlog.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/shop/ItemUtils.lua lib/ItemUtils.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/shop/oreExchanger.cfg /home/config/oreExchanger.cfg")
 shell.execute("wget -fq " .. REPOSITOTY .. "/shop/sellShop.cfg /home/config/sellShop.cfg")
