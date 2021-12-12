ae968d244acf73adddf8c421e1dc2e58079fa06f
Durex77 <salimabdul.it@gmail.com>
2021-12-12 23:01:17 +0200

    1

--- shop/shop.lua
+++ shop/shop.lua
@@ -281,20 +281,20 @@ function createLabelForm(labels, callback)
     local form = forms:addForm()
     form.border = 2
     form.W = 31
-    form.H = 10 + (#labels-1) * 2
+    form.H = 10 + (#labels-1) * 4
     form.left = math.floor((SellShopForm.W - form.W) / 2)
     form.top = math.floor((SellShopForm.H - form.H) / 2)
     local edits = {}
     for i = 1, #labels do
-        form:addLabel(8, 3 + (i-1) * 2, labels.label)
-        edits[i] = form:addEdit(8, 4 + (i-1) * 2)
+        form:addLabel(8, 3 + (i-1) * 4, labels[i].label)
+        edits[i] = form:addEdit(8, 4 + (i-1) * 4)
         edits[i].W = 18
     end
-    local backButton = form:addButton(3, 8, " Назад ", function()
+    local backButton = form:addButton(3, 8 + (#labels-1) * 4, " Назад ", function()
         form:setActive()
     end)
 
-    local acceptButton = form:addButton(17, 8, "Добавить", function()
+    local acceptButton = form:addButton(17, 8 + (#labels-1) * 4, "Добавить", function()
         local result = {}
         for i=1, #edits do
             table.insert(result, edits[i].text and edits[i].text or "")
@@ -342,7 +342,7 @@ function createSellShopForm()
     end)
 
     if (shopService:isAdmin(nickname)) then
-        local addButton = SellShopForm:addButton(70, 23, " Добавить ", function()
+        local addButton = SellShopForm:addButton(69, 23, " Добавить ", function()
             createLabelForm({{label=" Введите назву "}, {label=" Введите ID"}}, function (result)
                 shopService:addSellCategory(result[1], result[2])
                 createSellShopForm()
